4c0b8cf93ad7f7fdc24ee598507d00e9ba8f2b94
Jim Dios <jbryan3091@gmail.com>
2020-03-28 19:40:19 +0100

    todo_app v1

---
+++ components/ToDoList.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="to-do-container">
+    <div class="create-container">
+      <input v-model="titleOfNewToDo" type="text" />
+      <button @click="create">Add</button>
+    </div>
+    <div class="to-do-list-container">
+      <to-do-list-item v-for="todo in todos" :key="todo.id" :todo="todo" v-on:delete="deleteItem" />
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import ToDoListItem from "~/components/ToDoListItem.vue";
+
+export default {
+  components: {
+    ToDoListItem
+  },
+  props: {
+    todos: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      titleOfNewToDo: ""
+    };
+  },
+  methods: {
+    create() {
+      this.todos.push({
+        id: this.titleOfNewToDo,
+        title: this.titleOfNewToDo,
+        completed: false
+      });
+      this.titleOfNewToDo = "";
+    },
+    completeItem() {
+      this.todo.completed = true;
+    },
+    deleteItem() {
+      this.$emit("delete", this.todo);
+      this.todos.splice(this.todos.indexOf(item), 1);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.to-do-container {
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  display: block;
+
+  .create-container {
+    width: 100%;
+    height: 50px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+
+    input {
+      height: 50px;
+      width: 250px;
+      border: none;
+      padding-left: 10px;
+      font-family: "Quicksand", "Source Sans Pro", -apple-system,
+        BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+        sans-serif;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+      }
+    }
+
+    button {
+      height: 45px;
+      width: 145px;
+      border: none;
+      cursor: pointer;
+      text-align: right;
+      padding-right: 20px;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+.to-do-list-container {
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  display: block;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ pages/todo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h1 class="title">My To Do List</h1>
+    <to-do-list :todos="todos" />
+    <br />
+  </div>
+</template>
+
+<script>
+import ToDoList from "~/components/ToDoList.vue";
+
+export default {
+  components: {
+    ToDoList
+  },
+  data() {
+    return {
+      todos: {
+        type: Array
+      }
+    };
+  },
+  asyncData(ctx) {
+    return {
+      todos: [
+        {
+          id: "1",
+          title: "A",
+          completed: true
+        },
+        {
+          id: "2",
+          title: "B",
+          completed: false
+        },
+        {
+          id: "3",
+          title: "C",
+          completed: false
+        }
+      ]
+    };
+  }
+};
+</script>
